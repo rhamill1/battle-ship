3fc16ce35488f338ccc772de17ef410bed4734f8
rhamill1 <rhamill11@gmail.com>
2016-09-17 14:36:20 -0700

    hide, guess and check are working

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 window.onload = function() {
 
-console.log("it's loaded");
+
+    console.log("it's loaded");
 
 
-function hideBattleShip() {
-    // var battleShipLocation = eventlistener from ;
-
-    $("#submit-button").click(function() {
-      console.log('The submit button worked!!!');
-      var shipLocation = $("#battleShipInput").val()
-      console.log("I'm at " + shipLocation);
-    });
-
-}
+    function hideBattleShip() {
+         $("#submit-button").click(function() {
+          console.log('The submit button worked!!!');
+          var shipLocation = $("#battleShipInput").val()
+          console.log("I'm at " + shipLocation);
+          guessBattleShip(shipLocation)
+        });
+    }
 
 
-function guessBattleShip(battleShipLocation) {
-    this.battleShipLocation = battleShipLocation;
-    // var guess = user input;
+    function guessBattleShip(BattleShipCoordinates) {
+        this.BattleShipCoordinates = BattleShipCoordinates;
+         $("#guess-button").click(function() {
+          console.log('The guess button worked!!!');
+          var guessLocation = $("#fireTorpedo").val();
+          console.log("The ship's at " + BattleShipCoordinates);
+          console.log("FIRE torpedos at " + guessLocation);
+          checkGuess(guessLocation, BattleShipCoordinates);
+        });
+    }
 
-    // if
 
-}
-hideBattleShip();
+    hideBattleShip();
 
+
+    function checkGuess(guessBattleShip, hideBattleShip) {
+        this.guessBattleShip = guessBattleShip;
+        this.hideBattleShip = hideBattleShip;
+        if (guessBattleShip === hideBattleShip) {
+            console.log('you sunk my BattleShip');
+        }
+        else {
+            console.log("The torpedos missed.  Hurry and reload!!! We're being fired upon!!!")
+        }
+    }
 }
