c33567648be9767249366631c8900e9144a2e0f5
rhamill1 <rhamill11@gmail.com>
2016-09-17 23:14:36 -0700

    adds data validation and color pallet

--- app.js
+++ app.js
@@ -28,14 +28,17 @@ window.onload = function() {
     function checkGuess(guessBattleShip, hideBattleShip) {
         this.guessBattleShip = guessBattleShip;
         this.hideBattleShip = hideBattleShip;
-        matrixLookup = {'a': 'two', 'b': 'three', 'c': 'four', 'd': 'five', 'e': 'six', 'f': 'seven', 'g': 'eight'};
+        matrixLookup = {'a': 'two', 'b': 'three', 'c': 'four', 'd': 'five', 'e': 'six', 'f': 'seven', 'g': 'eight', 'h': 'nine'};
         if (guessBattleShip === hideBattleShip) {
             $("#return-text").html("<strong>Captain 2</strong>, you sunk Captain 1's battle ship!");
-            $("#row" + BattleShipCoordinates[1]).find("." + matrixLookup[BattleShipCoordinates[0]]).css( "background", "green" );
+            $("#row" + BattleShipCoordinates[1]).find("." + matrixLookup[BattleShipCoordinates[0]]).css( "background", "#ea391e" );
+        }
+        else if (guessBattleShip[1] > 4 || (!(guessBattleShip[0] in matrixLookup)) || guessBattleShip.length > 2) {
+            $("#return-text").html("Prepare the brig for Smee. He's had too much grog to man the torpedo hatch!");
         }
         else {
             $("#return-text").html("<strong>Captain 2</strong>, the torpedos missed.  Hurry and reload!!! We're being fired upon!!! Pick another coordinate and fire again!!!");
-            $("#row" + guessBattleShip[1]).find("." + matrixLookup[guessBattleShip[0]]).css( "background", "white" );
+            $("#row" + guessBattleShip[1]).find("." + matrixLookup[guessBattleShip[0]]).css( "background", "#667e94" );
         }
     }
 
